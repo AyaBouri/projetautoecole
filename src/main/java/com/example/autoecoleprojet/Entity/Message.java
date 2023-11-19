1b446faade4c88b64320d1cd1cbaa763e729adc3
AyaBouri <113023463+AyaBouri@users.noreply.github.com>
2023-11-19 13:13:23 +0100

    first commit

--- src/main/java/com/example/autoecoleprojet/Entity/Message.java
+++ src/main/java/com/example/autoecoleprojet/Entity/Message.java
@@ -1,2 +1,5 @@
-package com.example.autoecoleprojet.Entity;public class Message {
+package com.example.autoecoleprojet.Entity;
+import jakarta.persistence.Table;
+@Table(name = "message")
+public class Message {
 }
